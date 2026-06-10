13ba091cedb9ed2190fee483cd1e1a5004e30dee
Vedanshu Nishad <vedanshun05@gmail.com>
2026-06-10 10:05:21 +0530

    navbar

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -32,7 +32,7 @@ const About = () => {
         <h2 className="font-general text-sm uppercase md:text-[10px]">Welcome to Zentry</h2>
 
         <AnimatedTitle
-          title="Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared adventure"
+          title="Disc<b>o</b>ver the world's l<b>a</b>rgest <br /> shared adventure"
           containerClass="mt-5 !text-black text-center"
         />
 
