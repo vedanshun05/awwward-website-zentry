0e58810c3fe852978082b18268339fc65ff71ab6
Vedanshu Nishad <vedanshun05@gmail.com>
2026-06-10 11:55:05 +0530

    syntax update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -40,7 +40,7 @@ const Contact = () => {
 
           <AnimatedTitle
             title="let&#39;s b<b>u</b>ild the <br /> new era of <br /> g<b>a</b>ming t<b>o</b>gether."
-            className="special-font !md:text-[6.2rem] w-full font-zentry !text-5xl !font-black !leading-[.9]"
+            className="special-font md:text-[6.2rem]! w-full font-zentry text-5xl! font-black! leading-[.9]!"
           />
 
           <Button title="contact us" containerClass="mt-10 cursor-pointer" />
